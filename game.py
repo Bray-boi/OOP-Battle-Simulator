fc4888efc9a51b9fc5e5af40f8fc4f9f04e352bd
Bray-boi <0ppgam3rr@gmail.com>
2026-09-11 08:44:49 -0500

    Added second goblin

--- game.py
+++ game.py
@@ -13,6 +13,9 @@ def main():
     goblin = Goblin("Fred")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
+    secondGoblin = Goblin("Scribble")
+    
+    print(f"{secondGoblin.name} enters the arena with {secondGoblin.health} health.")
     print("But no hero has answered the call... yet.")
 
 
