beacc4b580784a46fb1d3e4d87bea6116c694702
Bray-boi <0ppgam3rr@gmail.com>
2026-09-14 12:34:17 -0500

    worked on hero class

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from goblin import Goblin
-
+from hero import Hero
 
 ARENA_NAME = "The Ring of Champions"
 
@@ -10,7 +10,9 @@ def main():
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ |_(`_`)_/")
     print("The gates are opening...")
 
+    hero = Hero("Jimminy")
     goblin = Goblin("Fred")
+
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     secondGoblin = Goblin("Scribble")
@@ -18,6 +20,15 @@ def main():
     print(f"{secondGoblin.name} enters the arena with {secondGoblin.health} health.")
     print("But no hero has answered the call... yet.")
 
+    print(f"{hero.name} steps into the arena with {hero.health} health.")
+    damage = hero.attack()
+    print(f"{hero.name} leaps forward striking {goblin.name} dealing {damage} damage!")
+    goblin.take_damage(damage)
+    if goblin.is_alive():
+        damage = goblin.attack()
+        print(f"Fred hits back!")
+        hero.take_damage(damage)
+        hero.is_alive()
 
 if __name__ == "__main__":
     main()
--- hero.py
+++ hero.py
@@ -1,4 +1,27 @@
+import random
 class Hero:
     """The hero blueprint will be implemented later in the project."""
 
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.health = 125
+        self.attack_power = 20
+        rollRace = random.randint(1, 20)
+        if rollRace <= 12:
+            race = "Human"
+        elif rollRace == 13:
+            race = "Angel"
+        elif rollRace > 13 and rollRace < 20:
+            race = "Shrek"
+
+        self.race = race
+
+    def attack(self):
+       return random.randint(1, self.attack_power)
+
+    def take_damage(self, damage):
+        self.health = max(0, self.health - damage)
+        print(f"{self.name} takes {damage} damage. Health: {self.health}")
+
+    def is_alive(self):
+        return self.health > 0
