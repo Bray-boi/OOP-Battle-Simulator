5c9a73524eb608029d709bc335e9149ec127c957
Bray-boi <0ppgam3rr@gmail.com>
2026-09-15 13:43:36 -0500

    Worked on main Game loop still need to add healing action and run action

--- game.py
+++ game.py
@@ -4,6 +4,25 @@ from hero import Hero
 ARENA_NAME = "The Ring of Champions"
 
 
+def battle(hero: Hero, enemy: Goblin):
+    while hero.is_alive() and enemy.is_alive():
+        print(f"What does {hero.name} do?")
+        print("1 Attack")
+        print("2 Heal")
+        print("3 Run")
+        hero_action = input("")
+        if hero_action == "1":
+            hero_damage = hero.attack()
+            enemy.take_damage(hero_damage)
+
+        if enemy.is_alive():
+            enemy_damage = enemy.attack()
+            hero.take_damage(enemy_damage)
+    if hero.is_alive():
+        print(f"{hero.name} wins!")
+    else:
+        print(f"{enemy.name} wins!")
+
 def main():
     """Open the arena and introduce its first opponent."""
     print(f"Welcome to {ARENA_NAME}!")
@@ -18,9 +37,9 @@ def main():
     secondGoblin = Goblin("Scribble")
     
     print(f"{secondGoblin.name} enters the arena with {secondGoblin.health} health.")
-    print("But no hero has answered the call... yet.")
+    
 
-    print(f"{hero.name} steps into the arena with {hero.health} health.")
+    print(f"{hero.name} steps into the arena with {hero.health} health and is of the {hero.race} race.")
     damage = hero.attack()
     print(f"{hero.name} leaps forward striking {goblin.name} dealing {damage} damage!")
     goblin.take_damage(damage)
@@ -29,5 +48,9 @@ def main():
         print(f"Fred hits back!")
         hero.take_damage(damage)
         hero.is_alive()
+
+    battle(hero, goblin)
+    if hero.is_alive():
+        battle(hero, secondGoblin)
 if __name__ == "__main__":
     main()
